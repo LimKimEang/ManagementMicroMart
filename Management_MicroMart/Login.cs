0cede1047354c486b864b9d26ca73c4790181a9a
LimKimEang <44645273+LimKimEang@users.noreply.github.com>
2020-12-22 15:20:22 +0700

    Add project files.

---
+++ Management_MicroMart/Login.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Management_MicroMart
+{
+    public partial class Login : Form
+    {
+        private string _UserName;
+        private string _Password;
+
+        public Login()
+        {
+            InitializeComponent();
+        }
+        public string login_Username
+        {
+            get { return this._UserName; }
+            set { this._UserName = value; }
+        }
+        public string login_Password
+        {
+            get { return this._Password; }
+            set { this._Password = value; }
+        }
+        private void Login_Load(object sender, EventArgs e)
+        {
+            txtUserName.Text = login_Username;
+            txtPassword.Text = login_Password;     
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (login_Password != "1234" || login_Username !="Staff")
+            {
+                MessageBox.Show("Please try again!");
+            }
+            else
+            {
+                MessageBox.Show("Login Successful");
+            }
+        }
+    }
+}
---
+++ Management_MicroMart/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Management_MicroMart")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Management_MicroMart")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("52350e62-cec2-41fe-a5a4-a1295451599b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
